58976de9caa14a86b0b3175a2dd621098038d8ba
Smith <qilong628@163.com>
2026-09-19 21:00:32 +0800

    test: define spring boot application baseline

---
+++ server/src/test/java/com/longscoop/ruankao/RuankaoApplicationTests.java
@@ -0,0 +1,12 @@
+package com.longscoop.ruankao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuankaoApplicationTests {
+
+    @Test
+    void applicationContextStarts() {
+    }
+}
