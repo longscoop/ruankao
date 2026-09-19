db9f60fd1997774468c00c842b181668e552c438
Smith <qilong628@163.com>
2026-09-19 21:42:31 +0800

    test: start postgres before spring context

--- server/src/test/java/com/longscoop/ruankao/support/PostgresIntegrationTest.java
+++ server/src/test/java/com/longscoop/ruankao/support/PostgresIntegrationTest.java
@@ -1,22 +1,20 @@
 package com.longscoop.ruankao.support;
 
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Testcontainers
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public abstract class PostgresIntegrationTest {
 
-    @Container
     protected static final PostgreSQLContainer<?> POSTGRES =
             new PostgreSQLContainer<>("postgres:16-alpine")
                     .withDatabaseName("ruankao_test")
                     .withUsername("ruankao")
                     .withPassword("ruankao");
+
+    static {
+        POSTGRES.start();
+    }
 
     @DynamicPropertySource
     static void databaseProperties(DynamicPropertyRegistry registry) {
